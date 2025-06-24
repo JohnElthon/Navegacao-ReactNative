8701e20e4b48146e1f3a2d85cfbde54278906c8e
JohnElthon <johnelthon1@gmail.com>
2025-06-24 11:44:25 -0300

    .

---
+++ Stack/pages/Home/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Home() {
+
+    const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>Página Home</Text>
+      <Button title='Ir para sobre' onPress={()=>navigation.navigate('Sobre')}></Button>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
