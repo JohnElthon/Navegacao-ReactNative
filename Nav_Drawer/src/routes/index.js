8ce2bcb3ed371cffe0374a05dcb0a25f2e58b6aa
JohnElthon <johnelthon1@gmail.com>
2025-07-30 11:24:18 -0300

    .

---
+++ Nav_Drawer/src/routes/index.js
@@ -0,0 +1,17 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Drawer = createDrawerNavigator();
+
+export default function Routes() {
+return (
+    <Drawer.Navigator>
+        <Drawer.Screen>
+            
+        </Drawer.Screen>
+
+    </Drawer.Navigator>
+  
+  );
+}
+
+
